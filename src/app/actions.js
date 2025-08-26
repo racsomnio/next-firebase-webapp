ec3269f52b0a3dc99d5ce7c519e30b274d4d830c
Racsomnio <oscar@blueswitch.com>
2025-08-25 20:48:33 -0400

    Fixed: Not adding review

--- src/app/actions.js
+++ src/app/actions.js
@@ -4,18 +4,17 @@ import { addReviewToRestaurant } from "@/src/lib/firebase/firestore.js";
 import { getAuthenticatedAppForUser } from "@/src/lib/firebase/serverApp.js";
 import { getFirestore } from "firebase/firestore";
 
-// This is a next.js server action, which is an alpha feature, so
-// use with caution.
+// This is a Server Action
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
 export async function handleReviewFormSubmission(data) {
-  const { app } = await getAuthenticatedAppForUser();
-  const db = getFirestore(app);
+  const { firebaseServerApp } = await getAuthenticatedAppForUser();
+  const db = getFirestore(firebaseServerApp);
 
   await addReviewToRestaurant(db, data.get("restaurantId"), {
-          text: data.get("text"),
-          rating: data.get("rating"),
+    text: data.get("text"),
+    rating: data.get("rating"),
 
-          // This came from a hidden form field.
-          userId: data.get("userId"),
+    // This came from a hidden form field
+    userId: data.get("userId"),
   });
 }
